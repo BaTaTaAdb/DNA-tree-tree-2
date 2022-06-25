861903ed3e06863bfe11a5d4aaae1dbad71e482c
João Nuno <49878220+BaTaTaAdb@users.noreply.github.com>
2022-06-25 18:35:45 +0000

    starting code importing dna

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 '''
 Enunciado: https://cs50.harvard.edu/x/2020/psets/6/dna/
 '''
+with open("sequences/11.txt","r") as texto:
+	dna = texto.readline()
+dna_split_AGATC = dna.split("AGATC")
+for i in dna_split_AGATC
