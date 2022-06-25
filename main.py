e12f564b4c44dc1dc9de5a3e31306d735097c237
BaTaTaAdb <joaominecraft.adr@gmail.com>
2022-06-25 23:26:15 +0100

    main

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 '''
 Enunciado: https://cs50.harvard.edu/x/2020/psets/6/dna/
 '''
-with open("sequences/11.txt","r") as texto:
+with open("sequences/1.txt","r") as texto:
 	dna = texto.readline()
-dna_split_AGATC = dna.split("AGATC")
-for i in dna_split_AGATC
+
+def dna_repeating_element(dna,element):
+	dna_split = dna.split(element)
+
+	consecutive_max = 0
+	consecutive = 0
+	for i in range(len(dna_split)-1):
+		if dna_split[i] == dna_split[i+1] == "" or dna_split[i+1] == "":
+			consecutive += 1
+		else:
+			if consecutive > consecutive_max:
+				consecutive_max = consecutive
+
+	return consecutive_max
