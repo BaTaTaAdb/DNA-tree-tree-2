49af5bbf2216e5d43d480ea8cd245d7070458aab
BaTaTaAdb <joaominecraft.adr@gmail.com>
2022-06-26 15:17:31 +0100

    main

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 '''
 Enunciado: https://cs50.harvard.edu/x/2020/psets/6/dna/
 '''
+from os import listdir
+from os.path import isfile, join
+import csv
+import sys
+args = sys.argv
 
 def dna_repeating_element(dna,element):
 	if element in dna:
 		dna_split = dna.split(element)
 		consecutive_max = 0
-		consecutive = 1
+		consecutive = 0
+		#print(dna_split)
 		for i in range(len(dna_split)-1):
 			if dna_split[i] == dna_split[i+1] == "" or dna_split[i+1] == "":
 				consecutive += 1
@@ -18,8 +24,29 @@ def dna_repeating_element(dna,element):
 	else:
 		return 0
 
+databases = [f for f in listdir("./databases/") if isfile(join("./databases/", f))]
+sequences = [f for f in listdir("./sequences/") if isfile(join("./sequences/", f))]
+#print(databases, sequences)
+if len(args) < 2:
+	print("ERROR: Invalid arguments count.\nUsage: python3 dna.py <data.csv> <sequence.txt>")
+	exit()
+"""if not (args[1] in databases and args[2] in sequences):
+	print("ERROR: Invalid arguments values.\nUsage: python3 dna.py <data.csv> <sequence.txt>")
+	exit()"""
 
-with open("sequences/1.txt","r") as texto:
+# get database
+with open(args[1], newline='') as database:
+	reader = csv.reader(database)
+	database_list = list(reader)
+
+# get dna from sequence argument
+with open(args[2],"r") as texto:
 	dna = texto.readline()
 
-print(dna_repeating_element(dna,"AGATC"))
+dna_elements_list = database_list[0][1::]
+for i in dna_elements_list:
+	pass
+
+
+
+#print(dna_repeating_element(dna,"AGATC"))
