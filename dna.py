92ad1450410a0586fe2dc1092babe469c237ec11
BaTaTaAdb <joaominecraft.adr@gmail.com>
2022-06-26 22:55:12 +0100

    main

--- dna.py
+++ dna.py
@@ -7,60 +7,64 @@ import csv
 import sys
 args = sys.argv
 
-def dna_repeating_element(dna,element):
-	if element in dna:
-		dna_split = dna.split(element)
-		consecutive_max = 0
-		consecutive = 1
-		#print(dna_split)
-		for i in range(len(dna_split)-1):
-			if dna_split[i] == dna_split[i+1] == "" or dna_split[i+1] == "":
-				consecutive += 1
-			else:
-				if consecutive > consecutive_max:
-					consecutive_max = consecutive
 
-		return consecutive_max
-	else:
-		return 0
+def dna_repeating_element(dna, element):
+    if element in dna:
+        dna_split = dna.split(element)
+        consecutive_max = 0
+        consecutive = 1
+        # print(dna_split)
+        for i in range(len(dna_split)-1):
+            if dna_split[i] == dna_split[i+1] == "" or dna_split[i+1] == "":
+                consecutive += 1
+            else:
+                if consecutive > consecutive_max:
+                    consecutive_max = consecutive
 
-databases = [f for f in listdir("./databases/") if isfile(join("./databases/", f))]
-sequences = [f for f in listdir("./sequences/") if isfile(join("./sequences/", f))]
+        return consecutive_max
+    else:
+        return 0
+
+
+databases = [f for f in listdir("./databases/")
+             if isfile(join("./databases/", f))]
+sequences = [f for f in listdir("./sequences/")
+             if isfile(join("./sequences/", f))]
 #print(databases, sequences)
 if len(args) < 2:
-	print("ERROR: Invalid arguments count.\nUsage: python3 dna.py <data.csv> <sequence.txt>")
-	exit()
+    print("ERROR: Invalid arguments count.\nUsage: python3 dna.py <data.csv> <sequence.txt>")
+    exit()
 """if not (args[1] in databases and args[2] in sequences):
 	print("ERROR: Invalid arguments values.\nUsage: python3 dna.py <data.csv> <sequence.txt>")
 	exit()"""
 
 # get database
 with open(args[1], newline='') as database:
-	reader = csv.reader(database)
-	database_list = list(reader)
+    reader = csv.reader(database)
+    database_list = list(reader)
 
 # get dna from sequence argument
-with open(args[2],"r") as texto:
-	dna = texto.readline()
+with open(args[2], "r") as texto:
+    dna = texto.readline()
 
 dna_elements_list = database_list[0][1::]
 person_dna = []
 for element in dna_elements_list:
-	repeated = dna_repeating_element(dna,element)
-	person_dna.append(repeated)
-	#print(f"{element}: {repeated}")
+    repeated = dna_repeating_element(dna, element)
+    person_dna.append(repeated)
+    #print(f"{element}: {repeated}")
 # print(person_dna)
 
 for i in database_list[1::]:
-	# print(i[1::])
-	test_list = [int(j) for j in i[1::]]
-	# print(test_list)
-	if test_list == person_dna:
-		print(i[0])
-		exit()
+    # print(i[1::])
+    test_list = [int(j) for j in i[1::]]
+    # print(test_list)
+    if test_list == person_dna:
+        print(i[0])
+        exit()
 print(person_dna)
 print("No match")
 
 
-#for dna in
-#print(dna_repeating_element(dna,"AGATC"))
+# for dna in
+# print(dna_repeating_element(dna,"AGATC"))
---
+++ test.py
@@ -0,0 +1,61 @@
+import subprocess
+from os import listdir, system
+from os.path import isfile, join
+import re
+import sys
+
+show_args = False
+if len(sys.argv) == 1:
+    show_args = input("Show args? ").lower() in ("y", "yes", "t", "true", "1")
+show_args_2 = sys.argv[1] != "noargs"
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
+
+
+databases = [f for f in listdir("./databases/")
+             if isfile(join("./databases/", f))]
+sequences = [f for f in listdir("./sequences/")
+             if isfile(join("./sequences/", f))]
+sequences.sort(key=natural_keys)
+expected_outcome = ["Bob", "No match", "No match", "Alice", "Lavender", "Luna", "Ron", "Ginny",
+                    "Draco", "Albus", "Hermione", "Lily", "No match", "Severus", "Sirius", "No match", "Harry", "No match", "Fred", "No match"]
+real_outcome = []
+
+for i in range(4):
+    proc = subprocess.Popen(['python', "dna.py",  f"databases/{databases[1]}", f"sequences/{sequences[i]}"],
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    communicate = str(proc.communicate()[0])
+    real_outcome.append(communicate)
+    print(
+        f"Test {i+1}: {communicate}\n args: databases/{databases[1]}; sequences/{sequences[i]}" if show_args or show_args_2 else "")
+for j in range(4, 20):
+    proc = subprocess.Popen(['python', "dna.py",  f"databases/{databases[0]}", f"sequences/{sequences[j]}"],
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    communicate = str(proc.communicate()[0])
+    real_outcome.append(communicate)
+    print(
+        f"Test {j+1}: {communicate}\n args: databases/{databases[0]}; sequences/{sequences[j]}" if show_args or show_args_2 else "")
+
+if show_args or show_args_2:
+    pass
+else:
+    system("cls")
+for i in range(len(real_outcome)):
+    if expected_outcome[i] in real_outcome[i]:
+        result = "Passed"
+    else:
+        result = "Failed"
+    print(f"\nTest {i+1}: {result}")
+    print(
+        f"Real Outcome: {real_outcome[i]}\nExpected Outcome: {expected_outcome[i]}" if result == "Failed" else f"{expected_outcome[i]}", end=" ")
